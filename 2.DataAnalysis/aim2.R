4c6a5c7f9cd773446186805ab3de421dd0a22ac7
jsperger <j.sperger@gmail.com>
2020-04-10 23:04:54 -0400

    First stab at Aim2 results. Be skeptical

--- 2.DataAnalysis/aim2.R
+++ 2.DataAnalysis/aim2.R
@@ -15,6 +15,28 @@ fitCTMLE <- function(study.data, outcome.name, cov.names, ...){
   return(ct.fit)
 }
 
+extractCTMLEFinalCovar <- function(ctmle.fit){
+  x <- summary(ctmle.fit)
+  # This code taken from print.summary.ctmle with minor modication
+  # For some reason CTMLE doesn't seem to have an easy way to let you see the covariates in the selected model
+  npercovar <- table(x$covar[1:x$ncand])
+  suffix <- paste(x$covar[1:x$ncand], letters[unlist(apply(npercovar, 1,function(x){1:x}))], sep="")
+  suffix[cumsum(npercovar)[npercovar==1]] <- names(cumsum(npercovar)[npercovar==1])
+  prev_covar <-suffix[cumsum(npercovar[-length(npercovar)])]
+  prev_moves <- c(paste(" + epsilon", prev_covar, " * h", prev_covar, sep=""),"")
+  current_move <- paste(" + epsilon", suffix, " * h", suffix,sep="")
+  TMLEcand <- paste("\tcand ", 1:x$ncand,": Q",1:x$ncand,"(A,W) =", sep="")
+  fluctuations <- paste("Q0(A,W)", c(rep("", npercovar[1]),
+                                     mapply(function(x){paste(prev_moves[1:(x-1)],collapse="")},x$covar[-(1:npercovar[1])])),
+                        current_move, sep="")
+  final_update <- c(rep("\n", npercovar[1]), paste("\t                = Q",
+                                                   rep(cumsum(npercovar[-length(npercovar)]), times=npercovar[-1]),"(A,W)",
+                                                   current_move[(npercovar[1]+1) : x$ncand],",\n", sep=""))
+  tx <- c("Intercept only",
+          paste(
+                mapply(function(x1){paste(x$terms[1:x1], collapse=", ")}, 1:(x$ncand-1)), sep=""))
+  return(tx[x$selected])
+}
 #########################################################
 ## AIPWE
 #########################################################
