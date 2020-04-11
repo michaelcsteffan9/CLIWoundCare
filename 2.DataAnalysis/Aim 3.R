3f2a9bde85fa8b471fbcf25601ddf62dabb79dc9
michaelcsteffan9 <59846866+michaelcsteffan9@users.noreply.github.com>
2020-04-11 04:50:52 -0400

    Update Aim 3.R

--- 2.DataAnalysis/Aim 3.R
+++ 2.DataAnalysis/Aim 3.R
@@ -2,15 +2,16 @@ library(randomForest)
 require(caTools)
 library(dplyr)
 library(tidyr)
+library(caret)
 
 #Data
 data <- read.csv("/Users/michael/Desktop/analyticData.csv", header=T)
-data_rf<-data[c(6:36,40)]
+data_rf<-data[c(3:7,10:40,44)]
 data_rf <- lapply(data_rf, factor)
-
-#Imputation
-set.seed(0408)
-data.imp<-rfImpute(ampFreeSurv2yr~ .,data_rf)
+data_rf$ageAtIndexProcedure=as.integer(data_rf$ageAtIndexProcedure)
+data_rf$maxRutherfordClass=factor(data_rf$maxRutherfordClass,ordered=T)
+data_rf$ischemia=factor(data_rf$ischemia,ordered=T)
+data_rf$woundClass=factor(data_rf$woundClass,ordered=T) 
 
 
 #Omitting
@@ -19,30 +20,35 @@ data.omit<-drop_na(data_rf)
 
 
 #Training Set(s)
-sample.imp = sample.split(data.imp$ampFreeSurv2yr, SplitRatio = .8)
-train.imp = subset(data.imp, sample.imp == TRUE)
-test.imp  = subset(data.imp, sample.imp == FALSE)
-
+set.seed(0412)
 sample.omit = sample.split(data.omit$ampFreeSurv2yr, SplitRatio = .8)
 train.omit = subset(data.omit, sample.omit == TRUE)
 test.omit  = subset(data.omit, sample.omit == FALSE)
 
 
-#Random Forest(s)
-rf.imp <- randomForest(ampFreeSurv2yr~ .,data=train.imp)
-rf.omit <- randomForest(ampFreeSurv2yr~ .,data=train.omit, na.action = na.pass)
+#randomforest Tuning
+X <- train.omit[,c(-37)]
+Y <- train.omit$ampFreeSurv2yr
+rf_tune<-tuneRF(X, Y, ntreeTry=500, stepFactor=1.25, improve=0.05, trace=T, plot=T,doBest=T)
+print(rf_tune)
+plot(rf_tune)
 
-importance(rf.imp)
+#CARET Tuning
+control <- trainControl(method="repeatedcv", number=10, repeats=3, search="random", p=1)
+rf_caret <- train(ampFreeSurv2yr~ .,data=train.omit, method="rf", metric='Accuracy', 
+                   tuneLength=10, trControl=control)
+print(rf_caret)
+plot(rf_caret)
+
+
+#Random Forest(s) and Importance
+rf.omit <- randomForest(ampFreeSurv2yr~ .,data=train.omit, na.action = na.pass, mtry=6)
 importance(rf.omit)
 
 
 #Predictions
-pred.imp = predict(rf.imp, test.imp)
 pred.omit = predict(rf.omit, test.omit)
 
 
 #Confusion Matrix (True Pos and True Neg)
-cm.imp = table(test.imp[,1], pred.imp)
-cm.omit = table(test.omit[,1], pred.imp)
-
-
+cm.omit = table(test.omit[,37], pred.omit)
